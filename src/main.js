dbd1618b7094b81f8688493a6b363845a39dc6d7
SergeyZasimov <zasimov.ser@yandex.ru>
2022-04-27 13:54:07 +0300

    Добавлены компоненты сортировки

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import { render, RenderPosition } from './render.js';
 import InfoView from './views/info-view.js';
 import FiltersView from './views/filters-view.js';
+import SortView from './views/sort-view.js';
 
 
 const headerContainer = document.querySelector('.page-header');
 const headerTripMain = headerContainer.querySelector('.trip-main');
 const headerControlsFilters = headerContainer.querySelector('.trip-controls__filters');
 
+const mainContainer = document.querySelector('.page-main');
+const tripEvents = mainContainer.querySelector('.trip-events');
+
 render(new InfoView(), headerTripMain, RenderPosition.AFTERBEGIN);
 render(new FiltersView(), headerControlsFilters);
+render(new SortView(), tripEvents);
