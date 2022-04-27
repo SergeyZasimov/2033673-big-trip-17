5e25467b246f8ba16639d090237bf0afa33355d3
SergeyZasimov <zasimov.ser@yandex.ru>
2022-04-27 15:06:01 +0300

    Создано представление события

--- src/presenter/events-presenter.js
+++ src/presenter/events-presenter.js
@@ -1,8 +1,11 @@
 import { render } from '../render.js';
 
 import EventsListView from '../views/events-list-view.js';
-import SortView from '../views/sort-view';
+import SortView from '../views/sort-view.js';
+import EventView from '../views/event-view.js';
 
+
+const eventsNumber = 3;
 
 export default class EventsPresenter {
   eventsListComponent = new EventsListView();
@@ -11,7 +14,10 @@ export default class EventsPresenter {
     this.eventsContainer = eventsContainer;
 
     render(new SortView(), this.eventsContainer);
-    render(this.eventsListComponent.getElement(), this.eventsContainer);
+    render(this.eventsListComponent, this.eventsContainer);
+    for (let i = 0; i < eventsNumber; i++) {
+      render(new EventView(), this.eventsListComponent.getElement());
+    }
   }
 
 }
