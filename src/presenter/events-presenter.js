e49a2aca1bdb638111697d0d79ff0080a1ddd685
SergeyZasimov <zasimov.ser@yandex.ru>
2022-04-27 15:26:11 +0300

    Создана форма редактирования события

--- src/presenter/events-presenter.js
+++ src/presenter/events-presenter.js
@@ -1,9 +1,10 @@
-import { render } from '../render.js';
+import { createElement, render } from '../render.js';
 
 import EventsListView from '../views/events-list-view.js';
 import SortView from '../views/sort-view.js';
 import EventView from '../views/event-view.js';
 import EventItemView from '../views/event-list-item.js';
+import EventEditView from '../views/event-edit-view.js';
 
 
 const eventsNumber = 3;
@@ -17,11 +18,16 @@ export default class EventsPresenter {
     render(new SortView(), this.eventsContainer);
     render(this.eventsListComponent, this.eventsContainer);
 
+    this.createEvent(new EventEditView());
+
     for (let i = 0; i < eventsNumber; i++) {
-      const listItem = new EventItemView();
-      render(listItem, this.eventsListComponent.getElement());
-      render(new EventView(), listItem.getElement());
+      this.createEvent(new EventView());
     }
   }
 
+  createEvent(component) {
+    const listItem = new EventItemView();
+    render(listItem, this.eventsListComponent.getElement());
+    render(component, listItem.getElement());
+  }
 }
