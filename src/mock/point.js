4e7ee04469c420601040b4572742d4391295d284
SergeyZasimov <zasimov.ser@yandex.ru>
2022-04-29 17:18:52 +0300

    Добавлена структура точки маршрута. Добавлен файл с утилитарными функциями. Добавлена функция генерации случайного числа.

---
+++ src/mock/point.js
@@ -0,0 +1,12 @@
+const generatePoint = () => ({
+  'base_price': 1100,
+  'date_from': '2019-07-10T22:55:56.845Z',
+  'date_to': '2019-07-11T11:22:13.375Z',
+  'destination': '',
+  'id': '0',
+  'is_favorite': false,
+  'offers': '',
+  'type': 'bus'
+});
+
+export { generatePoint };
