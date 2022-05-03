2891d795300a5d291289a5a7873e1006be76c125
SergeyZasimov <zasimov.ser@yandex.ru>
2022-05-03 18:33:39 +0300

    Замена точки маршрута на форму редактирования

--- src/presenter/events-presenter.js
+++ src/presenter/events-presenter.js
@@ -3,7 +3,6 @@ import { render } from '../render.js';
 import EventsListView from '../view/events-list-view.js';
 import SortView from '../view/sort-view.js';
 import EventView from '../view/event-view.js';
-import EventItemView from '../view/event-list-item.js';
 import EventEditView from '../view/event-edit-view.js';
 
 
@@ -19,16 +18,33 @@ export default class EventsPresenter {
     render(new SortView(), this.eventsContainer);
     render(this.#eventsListComponent, this.eventsContainer);
 
-    this.createEvent(new EventEditView(this.events[0]));
-
     for (let i = 0; i < this.events.length; i++) {
-      this.createEvent(new EventView(this.events[i]));
+      this.#renderEvent(this.events[i]);
     }
   }
 
-  createEvent(content) {
-    const listItem = new EventItemView();
-    render(listItem, this.#eventsListComponent.element);
-    render(content, listItem.element);
+  #renderEvent(event) {
+    const eventComponent = new EventView(event);
+    const eventEditComponent = new EventEditView(event);
+    render(eventComponent, this.#eventsListComponent.element);
+
+    const replaceEventToEdit = () => {
+      this.#eventsListComponent.element.replaceChild(eventEditComponent.element, eventComponent.element);
+    };
+
+    const replaceEditToEvent = () => {
+      this.#eventsListComponent.element.replaceChild(eventComponent.element, eventEditComponent.element);
+    };
+
+    eventComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
+      replaceEventToEdit();
+    });
+
+    eventEditComponent.element.querySelector('form').addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      replaceEditToEvent();
+    });
+
+
   }
 }
