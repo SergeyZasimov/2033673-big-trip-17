005f53c3e88ed5f029eb33e532d2bab798b4c33e
SergeyZasimov <zasimov.ser@yandex.ru>
2022-05-07 12:41:27 +0300

    Установлен фреймворк. Установлены лоадеры. Добавлено наследование простым классам.

--- src/mock/testSettings.js
+++ src/mock/testSettings.js
@@ -1,5 +1,5 @@
 const getTestSettings = () => ({
-  TEST_EVENTS_NUMBER: 0,
+  TEST_EVENTS_NUMBER: 5,
   TEST_DESTINATIONS: ['Amsterdam', 'Chamonix', 'Geneva'],
   TEST_EVENT_TYPES: ['taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'],
   TEST_DATE_FROM: ['2019-07-10T10:25:56.845Z', '2019-07-10T11:25:56.845Z'],
