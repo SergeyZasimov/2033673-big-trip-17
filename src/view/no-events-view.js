5d0831a50561e19c681f21de47900a7db00d1238
SergeyZasimov <zasimov.ser@yandex.ru>
2022-06-01 08:34:40 +0300

    Исправлена ошибка в компоненте заглушки

--- src/view/no-events-view.js
+++ src/view/no-events-view.js
@@ -3,8 +3,8 @@ import { FilterType } from '../utils/settings.js';
 
 const NoEventTextValue = {
   [FilterType.EVERYTHING]: 'Click New Event to create your first point',
-  [FilterType.FUTURE]: 'There are no past events now',
-  [FilterType.PAST]: 'There are no future events now',
+  [FilterType.PAST]: 'There are no past events now',
+  [FilterType.FUTURE]: 'There are no future events now',
 };
 
 const createNoEventsTemplate = (filterType) => {
